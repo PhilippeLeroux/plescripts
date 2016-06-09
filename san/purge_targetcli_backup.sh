1e7757c5f1e1ba8dd26202c330870313d70e65e9
PhilippeLeroux <philippe.lrx@gmail.com>
2016-06-09 16:22:20 +0200

    Purge les backups de target de plus de 7j.

---
+++ san/purge_targetcli_backup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+#	ts=4	sw=4
+
+. ~/plescripts/plelib.sh
+. ~/plescripts/global.cfg
+EXEC_CMD_ACTION=EXEC
+
+typeset -r ME=$0
+typeset -r str_usage=\
+"Usage : $ME ...."
+
+info "$ME $@"
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-emul)
+			EXEC_CMD_ACTION=NOOP
+			first_args=-emul
+			shift
+			;;
+
+		-db=*)
+			db=${1##*=}
+			shift
+			;;
+
+		-h|-help|help)
+			info "$str_usage"
+			LN
+			exit 1
+			;;
+
+		*)
+			error "Arg '$1' invalid."
+			LN
+			info "$str_usage"
+			exit 1
+			;;
+	esac
+done
+
+typeset -r targetcli_path=~/plescripts/san/targetcli_backup
+if [ ! -d $targetcli_path ]
+then
+	error "Directory $targetcli_path not exists !"
+	exit 1
+fi
+
+info "Supprime les sauvegardes de plus de 7 jours."
+exec_cmd "find $targetcli_path/* -mtime -7 -exec rm {} \;"
+LN
