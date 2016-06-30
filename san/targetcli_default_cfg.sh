7c250499ccaa0e3e5b21db58cfbfb7f0cd34412a
PhilippeLeroux <philippe.lrx@gmail.com>
2016-06-30 11:49:24 +0200

    Permet de configurer le SAN.
    Ce script a été crée pour ne plus dupliquer de code.

---
+++ san/targetcli_default_cfg.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#	ts=4	sw=4
+
+. ~/plescripts/plelib.sh
+. ~/plescripts/san/targetclilib.sh
+EXEC_CMD_ACTION=EXEC
+
+typeset -r ME=$0
+typeset -r str_usage=\
+"Usage : $ME ...."
+
+info "$ME $@"
+
+typeset db=undef
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-emul)
+			EXEC_CMD_ACTION=NOP
+			first_args=-emul
+			shift
+			;;
+
+		-h|-help|help)
+			info "$str_usage"
+			LN
+			exit 1
+			;;
+
+		*)
+			error "Arg '$1' invalid."
+			LN
+			info "$str_usage"
+			exit 1
+			;;
+	esac
+done
+
+set_targetcli_default_settings
+targetcli saveconfig
