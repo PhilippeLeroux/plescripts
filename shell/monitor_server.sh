2d922a99deebcf1a25eb2b524c3cc1ef4dbe5da3
PhilippeLeroux <philippe.lrx@gmail.com>
2016-07-03 11:23:25 +0200

    Utilisation de tmux pour afficher les stats.

--- shell/monitor_server.sh
+++ shell/monitor_server.sh
@@ -4,8 +4,9 @@
 EXEC_CMD_ACTION=EXEC
 
 #	Dimensiont du terminal
-typeset -r width=85
-typeset -r height=22
+typeset -r width_rac=174
+typeset -r height=48
+typeset -r width_single=$(( $width_rac / 2 ))
 
 #	Positions du terminal
 typeset -r top_right=+1024+0
@@ -29,32 +30,13 @@ fi
 typeset -r dir_files=~/plescripts/database_servers/$db
 [ ! -d $dir_files ] && error "$dir_files not exists." && exit 1
 
-function is_running # $1 server name
-{
-	ps -ef | grep xterm | grep -v grep | cut -d'@' -f2 | cut -d' ' -f1 | grep $1 2>&1 >/dev/null
-	echo $?
-}
-
-count_nodes=$(ls -1 $dir_files/node* | wc -l)
+typeset -ri count_nodes=$(ls -1 $dir_files/node* | wc -l)
 
 if [ $count_nodes -eq 2 ]
 then
-	if [ $(is_running srv${db}01) -ne 0 ]
-	then
-		xterm $xterm_static_options -geometry ${width}x${height}$top_left -rv -e "ssh root@srv${db}01 vmstat 2" &
-		xterm $xterm_static_options -geometry ${width}x${height}$bottom_left -rv -e "ssh -t root@srv${db}01 top" &
-	fi
-
-	if [ $(is_running srv${db}02) -ne 0 ]
-	then
-		xterm $xterm_static_options -geometry ${width}x${height}$top_right -rv -e "ssh root@srv${db}02 vmstat 2" &
-		xterm $xterm_static_options -geometry ${width}x${height}$bottom_right -rv -e "ssh -t root@srv${db}02 top" &
-	fi
+	xterm $xterm_static_options -geometry ${width_rac}x${height}$top_left +sb -rv \
+		-e "tmux_monitor_server.sh -node1=srv${db}01 -node2=srv${db}02" &
 else
-	if [ $(is_running srv${db}01) -ne 0 ]
-	then
-		xterm $xterm_static_options -geometry ${width}x${height}$top_right -rv -e "ssh root@srv${db}01 vmstat 2" &
-		xterm $xterm_static_options -geometry ${width}x${height}$bottom_right -rv -e "ssh -t root@srv${db}01 top" &
-	fi
+	xterm $xterm_static_options -geometry ${width_single}x${height}$top_left +sb -rv \
+		-e "tmux_monitor_server.sh -node1=srv${db}01" &
 fi
-
