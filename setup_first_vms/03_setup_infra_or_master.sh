de2bdba0a78527a47ec1eddc392b126d2e986a9c
PhilippeLeroux <philippe.lrx@gmail.com>
2016-07-08 13:11:55 +0200

    Oublie de désactiver target.

--- setup_first_vms/03_setup_infra_or_master.sh
+++ setup_first_vms/03_setup_infra_or_master.sh
@@ -206,6 +206,7 @@ case $role in
 		line_separator
 		info "Install workaround for target :"
 		exec_cmd "cp ~/plescripts/san/pletarget.service /usr/lib/systemd/system/"
+		exec_cmd systemctl disable target
 		exec_cmd systemctl enable pletarget
 		LN
 		;;
