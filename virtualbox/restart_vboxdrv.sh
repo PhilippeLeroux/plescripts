a302591205620fc32f54d45d4f33ac2993eceb56
PhilippeLeroux <philippe.lrx@gmail.com>
2016-07-12 23:45:08 +0200

    Permet de redémarrer vbox.

---
+++ virtualbox/restart_vboxdrv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#	ts=4	sw=4
+
+. ~/plescripts/plelib.sh
+. ~/plescripts/global.cfg
+EXEC_CMD_ACTION=EXEC
+
+typeset -r ME=$0
+typeset -r str_usage=\
+"Usage : $ME ...."
+
+info "$ME $@"
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-emul)
+			EXEC_CMD_ACTION=NOP
+			first_args=-emul
+			shift
+			;;
+
+		-h|-help|help)
+			info "$str_usage"
+			LN
+			exit 1
+			;;
+
+		*)
+			error "Arg '$1' invalid."
+			LN
+			info "$str_usage"
+			exit 1
+			;;
+	esac
+done
+
+exec_cmd "sudo systemctl stop vboxdrv"
+exec_cmd "sudo systemctl start vboxdrv"
+exec_cmd "~/plescripts/virtualbox/create_iface.sh"
+exec_cmd "sudo ifconfig"
