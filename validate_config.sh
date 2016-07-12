97d70f6d2dc0a66316a82d85b1ddf1b2d6cd614e
PhilippeLeroux <philippe.lrx@gmail.com>
2016-07-12 10:16:47 +0200

    Ajout des tests et mise en forme.

--- validate_config.sh
+++ validate_config.sh
@@ -40,49 +40,42 @@ do
 	esac
 done
 
-typeset -i bad_ip_host=0
-typeset -i plescripts_ok=0
-typeset -i zip_orcl_grid_ok=0
-typeset -i unzipped_orcl_grid_ok=0
-typeset -i iso_olinux_ok=0
-typeset -i resolv_conf_ok=0
-typeset -i vbox_is_ok=0
-typeset -i nc_is_ok=0
-typeset -i ssh_is_ok=0
+typeset -i count_errors=0
 
 line_separator
-info "L'addresse IP 192.170.100.1 correspond au poste client $client_hostname"
-info -n "Test l'existence de l'IP 192.170.100.1 : "
-ping -c 1 192.170.100.1 >/dev/null 2>&1
+info "L'addresse IP ${infra_network}.1 correspond au poste client $client_hostname"
+info -n "Test l'existence de l'IP ${infra_network}.1 "
+ping -c 1 ${infra_network}.1 >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
 	info -f "[$KO]"
-	error "Créer avec VirtualBox le réseau 192.170.100.1"
-	bad_ip_host=1
+	error "Créer avec VirtualBox le réseau ${infra_network}.0"
+	count_errors=count_errors+1
 else
 	info -f "[$OK]"
+	exec_cmd nslookup ${infra_network}.1
 fi
 LN
 
 line_separator
-info -n "Test l'existence de '$HOME/plescripts' : "
+info -n "Test l'existence de '$HOME/plescripts' "
 if [ ! -d "$HOME/plescripts" ]
 then
 	info -f "[$KO]"
 	error "	Ce répertoire doit contenir tous les scripts récupérés depuis GitHub."
-	plescripts_ok=1
+	count_errors=count_errors+1
 else
 	info -f "[$OK]"
 fi
 
 if [ $type_shared_fs == nfs ]
 then
-	info -n "Test l'existence de '$HOME/ISO/$oracle_install' : "
+	info -n "Test l'existence de '$HOME/ISO/$oracle_install' "
 	if [ ! -d "$HOME/ISO/$oracle_install" ]
 	then
 		info -f "[$KO]"
 		error "	Ce répertoire doit contenir les zips d'Oracle et du Grid."
-		zip_orcl_grid_ok=1
+		count_errors=count_errors+1
 	else
 		info -f "[$OK]"
 	fi
@@ -91,21 +84,21 @@ fi
 
 if [ $type_shared_fs == vbox ]
 then
-	info -n "Test l'existence de '$HOME/$oracle_install/database/runInstaller' : "
+	info -n "Test l'existence de '$HOME/$oracle_install/database/runInstaller' "
 	if [ ! -f "$HOME/$oracle_install/database/runInstaller" ]
 	then
 		info -f "[$KO]"
 		error "	Ce répertoire doit contenir les fichiers dézipés d'Oracle."
-		unzipped_orcl_grid_ok=1
+		count_errors=count_errors+1
 	else
 		info -f "[$OK]"
 	fi
-	info -n "Test l'existence de '$HOME/$oracle_install/grid/runInstaller' : "
+	info -n "Test l'existence de '$HOME/$oracle_install/grid/runInstaller' "
 	if [ ! -f "$HOME/$oracle_install/grid/runInstaller" ]
 	then
 		info -f "[$KO]"
 		error "	Ce répertoire doit contenir les fichiers dézipés du Grid."
-		unzipped_orcl_grid_ok=1
+		count_errors=count_errors+1
 	else
 		info -f "[$OK]"
 	fi
@@ -117,29 +110,39 @@ then
 	if [ $plescripts_ok -eq 0 ]
 	then
 		info "$client_hostname doit exporter via NFS les répertoires :"
-		info -n "	- $HOME/plescripts : "
+		info -n "	- $HOME/plescripts "
 		grep "$HOME/plescripts" /etc/exports >/dev/null 2>&1
-		plescripts_ok=$?
-		[ $plescripts_ok -ne 0 ] && info -f "[$KO]" || info -f "[$OK]"
+		if [ $? -ne 0 ]
+		then
+			count_errors=count_errors+1
+			info -f "[$KO]"
+		else
+			info -f "[$OK]"
+		fi
 	fi
 
 	if [ $zip_orcl_grid_ok -eq 0 ]
 	then
-		info -n "	- $HOME/ISO/$oracle_install : "
+		info -n "	- $HOME/ISO/$oracle_install "
 		grep "$HOME/ISO/$oracle_install" /etc/exports >/dev/null 2>&1
-		zip_orcl_grid_ok=$?
-		[ $zip_orcl_grid_ok -ne 0 ] && info -f "[$KO]" || info -f "[$OK]"
+		if [ $? -ne 0 ]
+		then
+			count_errors=count_errors+1
+			info -f "[$KO]"
+		else
+			info -f "[$OK]"
+		fi
 	fi
 	LN
 fi
 
 line_separator
-info -n "Test l'existence de $full_linux_iso_name : "
+info -n "Test l'existence de $full_linux_iso_name "
 if [ ! -f "$full_linux_iso_name" ]
 then
 	info -f "[$KO]"
 	error "L'ISO d'installation d'Oracle Linux 7 n'existe pas."
-	iso_olinux_ok=1
+	count_errors=count_errors+1
 else
 	info -f "[$OK]"
 fi
@@ -147,78 +150,68 @@ LN
 
 line_separator
 info "Validation de resolv.conf :"
-info -n " - nameserver $infra_ip : "
+info -n " - nameserver $infra_ip "
 if grep -q "^nameserver.*${infra_ip}" /etc/resolv.conf
 then
 	info -f "[$OK]"
 else
 	info -f "[$KO]"
-	resolv_conf_ok=1
+	count_errors=count_errors+1
 fi
 
-info -n " - search $infra_domain .* : "
+info -n " - search $infra_domain .* "
 if grep -q "^search.*${infra_domain}.*" /etc/resolv.conf
 then
 	info -f "[$OK]"
 else
 	info -f "[$KO]"
-	resolv_conf_ok=1
+	count_errors=count_errors+1
 fi
 LN
 
 line_separator
-info -n "Vbox dans le path : "
-if $(test_if_cmd_exists VBoxManage)
-then
-	info -f "[$OK]"
-else
-	vbox_is_ok=1
-	info -f "[$KO]"
-fi
-LN
-
-info -n "~/plescripts/shell dans le path : "
+info -n "~/plescripts/shell dans le path "
 if $(test_if_cmd_exists llog)
 then
 	info -f "[$OK]"
 else
-	info -f "${LBLUE}non${NORM}, fortement conseillé..."
-fi
-LN
-
-info -n "Vérifie si nc est installé : "
-if $(test_if_cmd_exists nc)
-then
-	info -f "[$OK]"
-else
 	info -f "[$KO]"
-	nc_is_ok=1
+	count_errors=count_errors+1
 fi
-LN
-
-info -n "Vérifie si ssh est installé : "
-if $(test_if_cmd_exists ssh)
-then
-	info -f "[$OK]"
-else
-	info -f "[$KO]"
-	ssh_is_ok=1
-fi
-LN
-
-info "Positionne les acls sur ~/plescripts"
-# Pour supprimer les acls : setfacl -Rb ~/plescripts/
-exec_cmd setfacl -Rm d:g:users:rwx $HOME/plescripts
 LN
 
 line_separator
-if [ $bad_ip_host -ne 0 ] 											\
-	|| [ $plescripts_ok -ne 0 ] || [ $zip_orcl_grid_ok -ne 0 ] 		\
-	|| [ $iso_olinux_ok -ne 0 ] || [ $unzipped_orcl_grid_ok -ne 0 ]	\
-	|| [ $resolv_conf_ok -ne 0 ] 									\
-	|| [ $vbox_is_ok -ne 0 ]										\
-	|| [ $nc_is_ok -ne 0 ] || [ $ssh_is_ok -ne 0 ]
+function in_path
+{
+	typeset -r cmd=$1
+
+	typeset -r msg=$(printf "%-10s " $cmd)
+	info -n "$msg"
+	if $(test_if_cmd_exists $cmd)
+	then
+		info -f "[$OK]"
+	else
+		count_errors=count_errors+1
+		info -f "[$KO]"
+	fi
+}
+
+in_path VBoxManage
+in_path nc
+in_path ssh
+in_path git
+LN
+
+line_separator
+info "Positionne les acls sur ~/plescripts"
+# Pour supprimer les acls : setfacl -Rb ~/plescripts/
+exec_cmd -c setfacl -Rm d:g:users:rwx $HOME/plescripts
+LN
+
+line_separator
+if [ $count_errors -ne 0 ]
 then
+	warning "$count_errors erreurs."
 	info "Corriger les erreurs avant de continuer."
 	exit 1
 else
