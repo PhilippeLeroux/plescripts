da8b24bd5efd9ba2da17d2c4996809899cf5a1fd
PhilippeLeroux <philippe.lrx@gmail.com>
2016-07-18 11:27:23 +0200

    Nettoyage.
    Les scripts trop vieux et/ou de tests sont supprimées.
    Réorganisation des fichiers.

--- oracle_preinstall/DB/paramSDBZZ4.sql
+++
@@ -1,1 +0,0 @@
-alter system set db_recovery_file_dest_size=2G scope=both;
--- oracle_preinstall/DB/test.sql
+++
@@ -1,8 +0,0 @@
---asm_preferred_read_failure_groups
-select
-	instance_name
-from
-	v$instance
-where
-	instance_number=1
-;
