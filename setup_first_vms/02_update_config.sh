892626cfe8f9d08386b12117c694cfce4034bc6b
PhilippeLeroux <philippe.lrx@gmail.com>
2016-07-21 12:57:57 +0200

    Mise à jour de l'OS pour le master.

--- setup_first_vms/02_update_config.sh
+++ setup_first_vms/02_update_config.sh
@@ -8,10 +8,17 @@ EXEC_CMD_ACTION=EXEC
 
 #Doit être exécuté sur le serveur d'infrastructure ou le master.
 
+line_separator
 . ~/plescripts/oracle_preinstall/make_vimrc_file
 make_vimrc_file "/root/.vimrc"
 
+line_separator
 cat ~/plescripts/setup_first_vms/for_inputrc /etc/inputrc > new_inputrc
 mv new_inputrc /etc/inputrc
 [ "$mode_vi" = "no" ] && sed -i "s/set editing-mode vi/#set editing-mode vi/" /etc/inputrc
 LN
+
+line_separator
+exec_cmd yum -y update
+LN
+
