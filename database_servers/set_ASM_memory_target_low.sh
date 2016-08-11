7a23674f10c9ba16d1d26a1953dc01a5d4475642
PhilippeLeroux <philippe.lrx@gmail.com>
2016-08-11 15:34:25 +0200

    La mémoire pour ASM est configurable.
    RAC uniquement.

--- database_servers/set_ASM_memory_target_low.sh
+++ database_servers/set_ASM_memory_target_low.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+. ~/plescripts/global.cfg
+
 sqlplus -s / as sysasm<<EOS
 alter system set "_asm_allow_small_memory_target"=true scope=spfile;
-alter system set memory_target=750m scope=spfile;
-alter system set memory_max_target=750m scope=spfile;
+alter system set memory_target=$hack_asm_memory scope=spfile;
+alter system set memory_max_target=$hack_asm_memory scope=spfile;
 exit
 EOS
-
