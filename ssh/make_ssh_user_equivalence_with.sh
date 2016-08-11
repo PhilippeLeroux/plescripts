1acff41aa7767a044cbf150735d787058d4b69ff
PhilippeLeroux <philippe.lrx@gmail.com>
2016-08-11 16:06:50 +0200

    Déplacement fichier puis création d'un lien.

---
+++ ssh/make_ssh_user_equivalence_with.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# vim: ts=4:sw=4
+
+. ~/plescripts/plelib.sh
+. ~/plescripts/networklib.sh
+EXEC_CMD_ACTION=EXEC
+
+typeset -r ME=$0
+typeset -r str_usage="Usage : $ME -user=<> -server=<>"
+
+typeset user=undef
+typeset server=undef
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-server=*)
+			server=${1##*=}
+			shift
+			;;
+
+		-user=*)
+			user=${1##*=}
+			shift
+			;;
+
+		*)
+			error "'$1' invalid."
+			LN
+			info $str_usage
+			LN
+			exit 1
+	esac
+done
+
+exit_if_param_undef user	$str_usage
+exit_if_param_undef server	$str_usage
+
+add_2_know_hosts $server
+LN
+
+typeset -r current_host=$(hostname -s)
+
+info "ssh connection $USER@$current_host / $user@$server"
+if [ ! -f ~/.ssh/id_rsa.pub ]
+then
+	info "Génération des clefs"
+	exec_cmd "[ ! -f ~/.ssh/id_rsa ] && ssh-keygen -t rsa -N \"\" -f ~/.ssh/id_rsa || true"
+else
+	info "Les clefs existent."
+fi
+LN
+
+info "Copy de la clef public"
+exec_cmd "ssh-copy-id -i ~/.ssh/id_rsa.pub ${user}@${server}"
+LN
