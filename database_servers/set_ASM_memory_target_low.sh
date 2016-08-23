227abe9b906f9115289a4769de6bb1466cbb404e
PhilippeLeroux <philippe.lrx@gmail.com>
2016-08-23 18:53:27 +0200

    BUG : oublie du sid='*'
    C'est peut être pour ça que je n'ai jamais vu l'intérêt de ces modifs.

--- database_servers/set_ASM_memory_target_low.sh
+++ database_servers/set_ASM_memory_target_low.sh
@@ -3,8 +3,7 @@
 . ~/plescripts/global.cfg
 
 sqlplus -s / as sysasm<<EOS
-alter system set "_asm_allow_small_memory_target"=true scope=spfile;
-alter system set memory_target=$hack_asm_memory scope=spfile;
-alter system set memory_max_target=$hack_asm_memory scope=spfile;
+alter system set "_asm_allow_small_memory_target"=true scope=spfile sid='*';
+alter system set memory_max_target=$hack_asm_memory scope=spfile sid='*';
 exit
 EOS
