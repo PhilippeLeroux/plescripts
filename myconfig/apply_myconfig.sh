98e63c557c15971d705bfd45465ff319d22ce136
PhilippeLeroux <philippe.lrx@gmail.com>
2016-09-14 20:59:14 +0200

    Réorganisation et maj de ma config.

--- myconfig/apply_myconfig.sh
+++ myconfig/apply_myconfig.sh
@@ -28,6 +28,36 @@ do
 done
 
 line_separator
+info "Config sudo for user $USER"
+info "Backup /etc/sudoers"
+exec_cmd sudo cp /etc/sudoers /etc/sudoers.backup
+LN
+
+typeset -i ln=$(sudo grep -n "root ALL=(ALL) ALL" /etc/sudoers | cut -d: -f1)
+ln=ln+1
+exec_cmd "sudo sed -i \"${ln}i\\$USER ALL=(root) NOPASSWD: ALL\" /etc/sudoers"
+LN
+
+exec_cmd -c "sudo visudo -c -f /etc/sudoers"
+if [ $? -ne 0 ]
+then
+	info "Broken file copied to /tmp/sudoers.broken"
+	exec_cmd "sudo cp /etc/sudoers.backup /tmp/sudoers.broken"
+	LN
+
+	info "Restore /etc/sudoers from backup"
+	exec_cmd "sudo mv /etc/sudoers.backup /etc/sudoers"
+	LN
+else
+	LN
+fi
+
+line_separator
+exec_cmd "cat ~/plescripts/setup_first_vms/for_inputrc /etc/inputrc > new_inputrc"
+exec_cmd "sudo mv new_inputrc /etc/inputrc"
+LN
+
+line_separator
 info "Apply bashrc extensions :"
 exec_cmd cp bashrc_extensions ~/.bashrc_extensions
 exec_cmd "sed -i \"/^.*bashrc_extensions.*$/d\" ~/.bashrc"
@@ -35,8 +65,14 @@ exec_cmd "echo \"[ -f ~/.bashrc_extensions ] && . ~/.bashrc_extensions || true\"
 LN
 
 line_separator
+exec_cmd "sudo zypper install git-core gvim"
+LN
+
+line_separator
 info "[G]vim configuration :"
 exec_cmd "~/plescripts/myconfig/vim_config.sh -restore"
+LN
+exec_cmd "~/plescripts/myconfig/vim_plugin.sh -init"
 LN
 
 line_separator
