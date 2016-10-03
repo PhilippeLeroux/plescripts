8f49186180b4ed4b3a4df3d4180a2d5f3d5e7aff
PhilippeLeroux <philippe.lrx@gmail.com>
2016-10-03 20:12:00 +0200

    Mutualisation de fonctions.

---
+++ gilib.sh
@@ -0,0 +1,43 @@
+#	Lib permettant d'exécuter sur tous les nœuds d'un RAC un script ou une commande.
+#	gi_current_node contient le nom du nœud courant.
+#	gi_node_list	contient le nom des autres nœuds pour un RAC.
+#	plelib.sh doit être chargée.
+# vim: ts=4:sw=4
+
+#	Retourne tous les nœuds du cluster moins le nœud courant.
+#	Si le serveur courant n'appartient pas à un cluster la fonction ne retourne rien.
+function get_other_nodes
+{
+	if $(test_if_cmd_exists olsnodes)
+	then
+		typeset nl=$(olsnodes | xargs)
+		if [ x"$nl" != x ]
+		then # olsnodes ne retourne rien sur un SINGLE
+			sed "s/$(hostname -s) //" <<<"$nl"
+		fi
+	fi
+}
+
+typeset -r gi_node_list=$(get_other_nodes)
+typeset -r gi_current_node=$(hostname -s)
+
+#	Exécute la commande "$@" sur tous les autres nœuds du cluster
+function execute_on_other_nodes
+{
+	typeset -r cmd="$@"
+
+	for node in $gi_node_list
+	do
+		exec_cmd "ssh -t -t $node '$cmd'</dev/null"
+	done
+}
+
+#	Exécute la commande "$@" sur tous les nœuds du cluster
+function execute_on_all_nodes
+{
+	typeset -r cmd="$@"
+
+	exec_cmd "$cmd"
+	execute_on_other_nodes "$cmd"
+}
+
