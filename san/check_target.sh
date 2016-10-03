2f04221488f95a1ed2528ba0a13554b40140ce64
PhilippeLeroux <philippe.lrx@gmail.com>
2016-10-03 20:06:00 +0200

    Meilleurs tests.

--- san/check_target.sh
+++ san/check_target.sh
@@ -4,16 +4,52 @@
 . ~/plescripts/plelib.sh
 EXEC_CMD_ACTION=EXEC
 
-typeset -ri count_lv_error=$(lvs 2>/dev/null| grep -E "*asm01 .*\-a\-.*$" | wc -l)
-if [ $count_lv_error -ne 0 ]
-then
-	info "LV errors : $count_lv_error"
-	LN
+function count_lv_errors
+{
+	lvs 2>/dev/null| grep -E "*asm01 .*\-a\-.*$" | wc -l
+}
+
+function show_lv_errors
+{
+	lvs 2>/dev/null| grep -E "*asm01 .*\-a\-.*$"
+}
+
+function restart_target
+{
+	info "Restart target :"
+
 	exec_cmd -c systemctl status target -l
 	LN
+	
 	exec_cmd systemctl start target
 	LN
+	
+	exec_cmd systemctl start target
+	LN
+	
 	exec_cmd systemctl status target -l
+	LN
+}
+
+typeset -i lv_errors=$(count_lv_errors)
+if [ $lv_errors -ne 0 ]
+then
+	info "LV errors : $lv_errors"
+	LN
+
+	restart_target
+
+	lv_errors=$(count_lv_errors)
+	if [ $lv_errors -ne 0 ]
+	then
+		error "$lv_errors LV(s) errors"
+		error "LV orphaned or bug ??"
+		LN
+
+		show_lv_errors
+		LN
+		exit 1
+	fi
 else
 	exit 0
 fi
