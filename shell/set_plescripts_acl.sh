8ca5644c9946861fa94f738fe78d75e5374299d5
PhilippeLeroux <philippe.lrx@gmail.com>
2016-10-05 00:01:19 +0200

    BUG : typo variable.

--- shell/set_plescripts_acl.sh
+++ shell/set_plescripts_acl.sh
@@ -18,7 +18,7 @@ info "Running : $ME $*"
 #	Si un fichier est créé sur un des serveurs utilisant l'export NFS alors il est
 #	possible de le manipuler sans trop de problèmes depuis le poste d'export.
 info "Positionne les acls sur ~/plescripts"
-exec_cmd -c setfacl -R -d -m u:{common_user_name}:rwx,g:users:rwx,o::r-x ~/plescripts/
+exec_cmd -c setfacl -R -d -m u:${common_user_name}:rwx,g:users:rwx,o::r-x ~/plescripts/
 if [ $? -ne 0 ]
 then
 	info "Vérifier que l'utilisateur $common_user_name et le groupe users existent."
