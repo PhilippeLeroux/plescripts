2288ee3a35d58907d7dae7a4efc335e67a7c01ba
PhilippeLeroux <philippe.lrx@gmail.com>
2016-10-04 21:22:08 +0200

    Script permettant d'activer les 'Huge Pages'.
    Fonctionne bien pour les single-instances, mais pour les RACs la RAM est
    un peu juste en particulier pour le nœud maitre.

---
+++ memory/orcl_setup_hp.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# vim: ts=4:sw=4
+
+. ~/plescripts/plelib.sh
+. ~/plescripts/dblib.sh
+. ~/plescripts/global.cfg
+EXEC_CMD_ACTION=EXEC
+
+typeset -r ME=$0
+typeset -r str_usage=\
+"Usage : $ME ...."
+
+info "Running : $ME $*"
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-emul)
+			EXEC_CMD_ACTION=NOP
+			first_args=-emul
+			shift
+			;;
+
+		-h|-help|help)
+			info "$str_usage"
+			LN
+			exit 1
+			;;
+
+		*)
+			error "Arg '$1' invalid."
+			LN
+			info "$str_usage"
+			exit 1
+			;;
+	esac
+done
+
+if [[ x"$ORACLE_SID" == x || "$ORACLE_SID" == NOSID ]]
+then
+	error "ORACLE_SID undef."
+	exit 1
+fi
+
+function slqcmd_setup_memory_hpages
+{
+	to_exec "alter system set memory_target=0 scope=spfile sid='*';"
+	to_exec "alter system set memory_max_target=0 scope=spfile sid='*';"
+	to_exec "alter system set sga_target=$shm_for_db scope=spfile sid='*';"
+}
+
+sqlplus_cmd "$(slqcmd_setup_memory_hpages)"
