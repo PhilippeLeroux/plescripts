fad2f1bc0b027d6d73c6e8dea1998b85b76de0bb
PhilippeLeroux <philippe.lrx@gmail.com>
2016-10-20 12:57:43 +0200

    Ajout du quota sur le tablespace de l'utilisateur.

--- db/sql/create_user.sql
+++ db/sql/create_user.sql
@@ -5,54 +5,54 @@ define tbs='&username.tbs'
 
 set serveroutput on size unlimited
 declare
-	LN constant char(1) := chr(10);
+LN constant char(1) := chr(10);
 
-	--	Constantes pour la fonction exec :
-	on_error_raise		constant pls_integer := 1;
-	on_error_continue	constant pls_integer := 2;
+--	Constantes pour la fonction exec :
+on_error_raise		constant pls_integer := 1;
+on_error_continue	constant pls_integer := 2;
 
-	--
-	procedure p( b varchar2 ) as
-	begin
-		dbms_output.put_line( b );
-	end p;
+--
+procedure p( b varchar2 ) as
+begin
+	dbms_output.put_line( b );
+end p;
 
-	--
-	function exist_tbs( tbs_name varchar2 ) return boolean as
-		foo dba_tablespaces.tablespace_name%type;
-	begin
-		select
-			tablespace_name
-		into
-			foo
-		from
-			dba_tablespaces
-		where
-			tablespace_name=upper( tbs_name )
-		;
+--
+function exist_tbs( tbs_name varchar2 ) return boolean as
+	foo dba_tablespaces.tablespace_name%type;
+begin
+	select
+		tablespace_name
+	into
+		foo
+	from
+		dba_tablespaces
+	where
+		tablespace_name=upper( tbs_name )
+	;
 
-		return true;
+	return true;
 
-	exception
-		when no_data_found	then return false;
-		when others			then raise;
-	end exist_tbs;
+exception
+	when no_data_found	then return false;
+	when others			then raise;
+end exist_tbs;
 
-	--
-	procedure exec( cmd varchar2, on_error pls_integer default on_error_raise ) as
-	begin
-		p( '> '||cmd||';' );
-		execute immediate cmd;
-		p( '-- success.'||LN );
-	exception
-		when others then
-			if on_error = on_error_raise then
-				p( '-- Failed : '||sqlerrm||LN );
-				raise;
-			else
-				p( '-- Warning : '||sqlerrm||LN );
-			end if;
-	end exec;
+--
+procedure exec( cmd varchar2, on_error pls_integer default on_error_raise ) as
+begin
+	p( '> '||cmd||';' );
+	execute immediate cmd;
+	p( '-- success.'||LN );
+exception
+	when others then
+		if on_error = on_error_raise then
+			p( '-- Failed : '||sqlerrm||LN );
+			raise;
+		else
+			p( '-- Warning : '||sqlerrm||LN );
+		end if;
+end exec;
 
 begin
 	exec( 'drop user &username cascade', on_error_continue );
@@ -65,5 +65,6 @@ begin
 	exec( 'alter profile default limit password_life_time unlimited' );
 	exec( 'create user &username identified by &username default tablespace &tbs' );
 	exec( 'grant create session, create table to &username' );
+	exec( 'alter user &username quota unlimited on  &tbs' );
 end;
 /
