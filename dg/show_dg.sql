684c52839df5f1284b2a09d64f87f609273fae8f
PhilippeLeroux <philippe.lrx@gmail.com>
2016-10-24 15:47:48 +0200

    Ajustement largeur colonnes.

--- dg/show_dg.sql
+++ dg/show_dg.sql
@@ -1,8 +1,8 @@
 set lines 130 pages 500
 break on dg_name on failgroup skip 1
 col dg_name for a12
-col name for a12
-col label for a12
+col name for a14
+col label for a14
 col path for a20
 select
     dg.name "dg_name"
