c78a4bac8ab0a2bfab43e7281e748de2a74cc119
PhilippeLeroux <philippe.lrx@gmail.com>
2017-04-28 09:15:16 +0200

    Ajout scripts crosscheck et backup archivelogs

---
+++ db/backup_archivelogs.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# vim: ts=4:sw=4
+
+. ~/plescripts/plelib.sh
+. ~/plescripts/dblib.sh
+. ~/plescripts/global.cfg
+EXEC_CMD_ACTION=EXEC
+
+typeset -r ME=$0
+typeset -r str_usage=\
+"Usage : $ME"
+
+script_banner $ME $*
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-emul)
+			EXEC_CMD_ACTION=NOP
+			first_args=-emul
+			shift
+			;;
+
+		-h|-help|help)
+			info "$str_usage"
+			LN
+			exit 1
+			;;
+
+		*)
+			error "Arg '$1' invalid."
+			LN
+			info "$str_usage"
+			exit 1
+			;;
+	esac
+done
+
+exit_if_ORACLE_SID_not_defined
+
+script_start
+
+if command_exists crsctl
+then
+	typeset -r crs_used=yes
+else
+	typeset -r crs_used=no
+fi
+
+exec_cmd cd ~/plescripts/db/rman
+LN
+
+if [ $crs_used == no ]
+then
+	typeset -r disk_space_before="$(df -h /u0*)"
+fi
+
+line_separator
+exec_cmd "rman target sys/$oracle_password @backup_archive_log.rman"
+LN
+
+exec_cmd "cd -"
+LN
+
+if [ $crs_used == no ]
+then
+	line_separator
+	info "Espace disque avant backup :"
+	echo "$disk_space_before"
+	LN
+
+	info "Espace disque après backup :"
+	exec_cmd "df -h /u0*"
+	LN
+fi
+
+script_stop ${ME##/*}
