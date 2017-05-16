dc773e6e60c5f93ee8d99b0a376cf429033ebd2c
PhilippeLeroux <philippe.lrx@gmail.com>
2017-05-16 13:02:52 +0200

    Ajout d'un commentaire sur le Wallet Manager
    Pour résumer, le script root.sh créé un chemin spécifique pour stocker
    le Wallet, mais je ne peux pas l'utiliser.

--- db/wallet/walletlib.sh
+++ db/wallet/walletlib.sh
@@ -1,5 +1,11 @@
 # vim: ts=4:sw=4
 
+# Le script /u02/app/oracle/12.2.0.1/dbhome_1/install/root_schagent.sh exécuté
+# par le script root.sh après l'installation d'Oracle créé le répertoire :
+# $ORACLE_HOME/data/wallet
+# Mais mes scripts ne peuvent pas l'utiliser car si le répertoire existe je
+# considère que le wallet est installé.
+# Note : le script Oracle positionne les droits 700 sur le répertoire.
 typeset -r wallet_path=${ORA_WALLET-$ORACLE_HOME/oracle/wallet}
 
 #*> return 0 if wallet on CFS else return 1
