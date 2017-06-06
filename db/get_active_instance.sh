e4ba9a32cd28323c7c2ed0a8121d82f02b3600b9
PhilippeLeroux <philippe.lrx@gmail.com>
2017-06-06 18:43:31 +0200

    Fix check instance Oracle
    Si le nom d'une instance possédait "asm" les scripts échouaient.

--- db/get_active_instance.sh
+++ db/get_active_instance.sh
@@ -2,7 +2,7 @@
 # vim: ts=4:sw=4
 
 #	-f3-4 pour gérer le cas des RAC One Node ou Policy Managed.
-instance=$(ps -ef |  grep [p]mon | grep -vE "MGMTDB|ASM" | cut -d_ -f3-4)
+instance=$(ps -ef |  grep [p]mon | grep -vE "MGMTDB|\+ASM" | cut -d_ -f3-4)
 if [ x"$instance" != x ]
 then
 	echo "$instance"
