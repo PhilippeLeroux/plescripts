a0f18267671602d018870c732899fef1fe6f1b8b
PhilippeLeroux <philippe.lrx@gmail.com>
2017-10-19 12:33:37 +0200

    Fix et lecture de la version de LEAP

--- myconfig/h264_tumbleweed.sh
+++ myconfig/h264_tumbleweed.sh
@@ -6,16 +6,14 @@ EXEC_CMD_ACTION=EXEC
 
 if [ "$(cat /etc/os-release | grep "^NAME" | cut -d\" -f2)" == "openSUSE Leap" ]
 then
-	exec_cmd sudo zypper addrepo -f http://packman.inode.at/suse/openSUSE_Leap_42.3/ packman
-	LN
-
-	exec_cmd sudo zypper addrepo -f http://opensuse-guide.org/repo/openSUSE_Leap_42.2/ dvd
+	typeset	version=$(cat /etc/os-release | grep "^VERSION=" | cut -d\" -f2)
+	exec_cmd sudo zypper addrepo -f http://packman.inode.at/suse/openSUSE_Leap_$version/ packman
 	LN
 
 	exec_cmd sudo zypper install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2
 	LN
 
-	exec_cmd sudo zypper dup --from http://packman.inode.at/suse/openSUSE_Leap_42.2/
+	exec_cmd sudo zypper dup --from http://packman.inode.at/suse/openSUSE_Leap_$version/
 	LN
 else
 	exec_cmd sudo zypper addrepo -f http://packman.inode.at/suse/openSUSE_Tumbleweed/ packman
