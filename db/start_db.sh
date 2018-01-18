6f8cc686b07948a741441e5414537ec829a86efd
PhilippeLeroux <philippe.lrx@gmail.com>
2018-01-18 15:36:08 +0100

    Test spécifique RAC Policy Managed
    - Pour les bases RAC Policy Managed prévient si l'instance change.
    - Pour les bases RAC Policy Managed si l'instance change
      l'environnement est rechargé pour que le script lspdbs fonctionne.
    - Si le Grid Infra est installé, il n'est plus nécessaire que
      l'environnement soir chargé.

--- db/start_db.sh
+++ db/start_db.sh
@@ -43,24 +43,61 @@ done
 
 #ple_enable_log -params $PARAMS
 
+if command_exists crsctl
+then
+	typeset	-r	crs_used=yes
+else
+	typeset	-r	crs_used=no
+fi
+
 if [ "$oracle_sid" == undef ]
 then
-	exit_if_ORACLE_SID_not_defined
+	[ $crs_used == no ] && exit_if_ORACLE_SID_not_defined || true
 else
 	export ORACLE_SID=$oracle_sid
 	ORAENV_ASK=NO . oraenv
 fi
 
-if command_exists crsctl
+typeset	-r	instance=$ORACLE_SID
+
+if [ $crs_used == yes ]
 then
-	typeset -r db_name=$(srvctl config database)
+	typeset -r	db_name=$(srvctl config database)
 
 	exec_cmd srvctl start database -db $db_name
 	LN
+
+	if [[ x"$ORACLE_SID" == x || "$ORACLE_SID" == NOSID ]]
+	then # Pour que le script lspdbs fonctionne.
+		export ORACLE_SID=$(~/plescripts/db/get_active_instance.sh)
+		info "Load Oracle environment for $ORACLE_SID"
+		ORAENV_ASK=NO . oraenv
+		LN
+	fi
 else
 	sqlplus_cmd "$(set_sql_cmd "startup")"
 	LN
 fi
 
+typeset		warning_msg
+if [[ x"$ORACLE_SID" != x && "$ORACLE_SID" != NOSID && x"$instance" != x && "$instance" != NOSID ]]
+then
+	ORACLE_SID=$(~/plescripts/db/get_active_instance.sh)
+	# Dans le cas d'un RAC Policy Managed l'instance peut changer.
+	if [[ "$instance" != "$ORACLE_SID" ]]
+	then
+		warning_msg="${BLINK}Instance${NORM} has been changed $ORACLE_SID became $instance"
+		# Permet au script lspdbs de fonctionner.
+		export ORACLE_SID=$instance
+		ORAENV_ASK=NO . oraenv
+	fi
+fi
+
 sqlplus_cmd "$(set_sql_cmd "@$HOME/plescripts/db/sql/lspdbs.sql")"
 LN
+
+if [ x"$warning_msg" != x ]
+then
+	warning "$warning_msg"
+	LN
+fi
